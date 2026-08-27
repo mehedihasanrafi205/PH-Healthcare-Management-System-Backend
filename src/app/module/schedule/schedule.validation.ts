a7acf175f4c314496d3cdaf16384a68641c1ebfa
Mehedi Hasan Rafi <mehedihasanrafi205@gmail.com>
2026-08-28 03:56:41 +0600

    feat: add schedule module with controller service interface and create schedule api
    - Install date-fns dependency for date and time calculations
    - Add service and validation logic for creating doctor schedules
    - Implement date overlap and schedule conflict validation for doctors
    - Generate time slots and store doctor schedule details in database

---
+++ src/app/module/schedule/schedule.validation.ts
@@ -0,0 +1,83 @@
+import { addDays, differenceInMinutes, startOfDay } from "date-fns";
+import { prisma } from "../../lib/prisma";
+import { RequestUser } from "../../middleware/checkAuth";
+import { AppError } from "../../utils/AppError";
+import { ICreateSchedulePayload } from "./schedule.interface";
+import httpStatus from "http-status";
+
+const createSchedule = async (
+  payload: ICreateSchedulePayload,
+  user: RequestUser,
+) => {
+  const doctor = await prisma.doctor.findUnique({
+    where: { userId: user.userId },
+  });
+
+  if (!doctor) {
+    throw new AppError(httpStatus.NOT_FOUND, "Doctor Profile Not Found");
+  }
+
+  const startOfTheDay = startOfDay(payload.startDateTime);
+
+  const startOfNextDay = addDays(startOfTheDay, 1);
+
+  const existingScheduleOnThisDate = await prisma.schedule.findFirst({
+    where: {
+      doctorId: doctor.id,
+      isDeleted: false,
+      startDateTime: {
+        gte: startOfTheDay,
+        lt: startOfNextDay,
+      },
+    },
+  });
+
+  if (existingScheduleOnThisDate) {
+    throw new AppError(
+      httpStatus.CONFLICT,
+      "You Already Have A Schedule For This Date",
+    );
+  }
+
+  const durationInMinutes = differenceInMinutes(
+    payload.endDateTime,
+    payload.startDateTime,
+  );
+
+  const MINUTES_ALLOCATED_PER_SLOT = 20;
+
+  const totalSlots = Math.floor(durationInMinutes / MINUTES_ALLOCATED_PER_SLOT);
+
+  if (totalSlots < 1) {
+    throw new AppError(
+      httpStatus.CONFLICT,
+      `Schedule Must Be At Least ${MINUTES_ALLOCATED_PER_SLOT} Minutes Long To Fit One Slot`,
+    );
+  }
+
+  const schedule = await prisma.schedule.create({
+    data: {
+      startDateTime: payload.startDateTime,
+      endDateTime: payload.endDateTime,
+      meetingLink: payload.meetingLink,
+      totalSlots,
+      availableSlots: totalSlots,
+      doctorId: doctor.id,
+    },
+    include: {
+      doctor: {
+        select: {
+          name: true,
+          email: true,
+          contactNumber: true,
+        },
+      },
+    },
+  });
+
+  return schedule;
+};
+
+export const ScheduleServices = {
+  createSchedule,
+};
