dcb7b8af1b26901c8208670964900f5e2ecdedde
Mehedi Hasan Rafi <mehedihasanrafi205@gmail.com>
2026-08-30 14:27:13 +0600

    feat: implement apis for getting my payments, all payments, and single payment
    - Add endpoint for logged-in users to retrieve their payment history (get my payments)
    - Create admin service and controller logic to fetch all payments with filtering and pagination
    - Implement endpoint to retrieve specific payment transaction details by ID

--- src/app.ts
+++ src/app.ts
@@ -19,6 +19,7 @@ import { getBkashIdToken } from "./app/lib/bkash";
 import { AppointmentRoutes } from "./app/module/appointment/appointment.route";
 import { DoctorRoutes } from "./app/module/doctor/doctor.route";
 import { ScheduleRoutes } from "./app/module/schedule/schedule.route";
+import { PaymentRoutes } from "./app/module/payment/payment.route";
 
 
 const app: Application = express();
@@ -42,6 +43,7 @@ app.use("/api/v1/user", UserRoutes);
 app.use("/api/v1/appointment", AppointmentRoutes);
 app.use("/api/v1/doctor", DoctorRoutes);
 app.use("/api/v1/schedule", ScheduleRoutes);
+app.use("/api/v1/payment", PaymentRoutes);
 
 app.get("/test", async (req: Request, res: Response, next: NextFunction) => {
   try {
